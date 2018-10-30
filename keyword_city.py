fb9474c0d736a4e8e701a71efdcb5d8e5a5de9e2
diW <di.W@hotmail.com>
2018-10-31 01:21:18 +0800

    statics of keyword corpus scale

--- keyword_city.py
+++ keyword_city.py
@@ -19,5 +19,9 @@ weibofilefolder = 'D:/chinadream/data'
 
 
 if __name__ == '__main__':
-    file_path_list = topic_sta1.getAllFile(weibofilefolder)
-    topic_sta1.collect_city_file(file_path_list)
+
+    # file_path_list = topic_sta1.getAllFile(weibofilefolder)
+    # topic_sta1.collect_city_file(file_path_list)
+
+    #统计各keyword 和城市的微博语料数
+    topic_sta1.calc_city_doc()
