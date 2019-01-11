4aeb40470ad35bffbbc93c9e0b25c62ca2f724d0
diweng <di.w@hotmail.com>
2019-01-11 13:03:50 +0800

    remove #

--- temo.py
+++ temo.py
@@ -38,8 +38,10 @@ def extract_word_freq(file_folder_path):
     with open('data/stop_word.txt', 'r', encoding='utf-8') as sw_f:
         for item in sw_f:
             stop_word.append(item.strip())
-
     current_keyword = file_folder_path.split('/')[-1]
+    if(current_keyword != '健康'):
+        return
+    print(current_keyword)
     current_keyword_cut_list = current_keyword.split(',')
     keyword_ban_list = [ban_word for ban_word in current_keyword_cut_list]
     for ban_word in current_keyword_cut_list:
@@ -62,6 +64,7 @@ def extract_word_freq(file_folder_path):
                 weibo_cut = list(jieba.cut(weibo_origin))
                 for items in weibo_cut:
                     if ((items not in stop_word) and (items not in keyword_ban_list) and len(items.strip()) > 0):
+                        print(items)
                         weibo_cut_list.append(items)
                 line_count += 1
                 if(line_count >= 10000):
@@ -79,53 +82,32 @@ def extract_word_freq(file_folder_path):
             sort_dict = sorted(keyword_out_dic.items(), key=lambda d: d[1], reverse=True)
 
             temp_count = 0
-            outfile = open(out_folder+current_keyword,'a+',encoding='utf-8')
-
-            for current_tuple in sort_dict:
-                temp_count += 1
-                outfile.write(str(current_tuple[0]))
-                outfile.write(' ')
-                outfile.write(str(current_tuple[1]))
-                outfile.write(' ')
-                if(temp_count >= 50):
-                    break
-            outfile.write('\n')
-            outfile.close()
+            # outfile = open(out_folder+current_keyword,'a+',encoding='utf-8')
+            #
+            # for current_tuple in sort_dict:
+            #     temp_count += 1
+            #     outfile.write(str(current_tuple[0]))
+            #     outfile.write(' ')
+            #     outfile.write(str(current_tuple[1]))
+            #     outfile.write(' ')
+            #     if(temp_count >= 50):
+            #         break
+            # outfile.write('\n')
+            # outfile.close()
             # fd.plot(50,cumulative=True)
 
-def exec_shell_train():
-    wordFreq_file_path = 'data/location_wordfreq/'
-    wordFreq_keyword_list = os.listdir(wordFreq_file_path)
-    for current_keyword in wordFreq_keyword_list:
-        if(current_keyword == 'lda_folder'):
-            continue
-        print(current_keyword)
-        shell_command = '/Users/diweng/github_project/plda/lda --num_topics 8 --alpha 0.1 --beta 0.01 --training_data_file /Users/diweng/github_project/ChinaDream/data/location_wordfreq/' + current_keyword + ' --model_file /Users/diweng/github_project/ChinaDream/data/location_wordfreq/lda_folder/' + current_keyword + '.txt --burn_in_iterations 100 --total_iterations 150'
-        os.system(shell_command)
-def exec_shell_view():
-    model_folder_path = 'data/location_wordfreq/lda_folder/'
-    model_file_list = os.listdir(model_folder_path)
-    for current_model in model_file_list:
-        shell_command = '/Users/diweng/github_project/plda/view_model.py /Users/diweng/github_project/ChinaDream/' + model_folder_path + current_model
-        print(current_model)
-        os.system(shell_command)
+
 if __name__=='__main__':
+     #lda
+     # topic_sta1.keyword_location_lda()
 
-    # lda
-    # topic_sta1.keyword_location_lda()
 
-    #输出结果 失败
-    # topic_sta1.lda_city_topic()
+     # 抽取plda所需词频文件
+     weibocityfilefolder = '/Volumes/data/chinadream/keyword_location/'
+     keyword_folder_name = os.listdir(weibocityfilefolder)
+     for current_keyword_name in keyword_folder_name:
+         current_keyword_folder = weibocityfilefolder + current_keyword_name
+         extract_word_freq(current_keyword_folder)
 
-    # 抽取plda所需词频文件
-    # weibocityfilefolder = '/Volumes/data/chinadream/keyword_location/'
-    # keyword_folder_name = os.listdir(weibocityfilefolder)
-    # for current_keyword_name in keyword_folder_name:
-    #     current_keyword_folder = weibocityfilefolder + current_keyword_name
-    #     extract_word_freq(current_keyword_folder)
-
-    # shell_训练
-    # exec_shell_train()
-
-    #shell_查看话题
-    exec_shell_view()
+     #输出结果
+     # topic_sta1.lda_city_topic()
